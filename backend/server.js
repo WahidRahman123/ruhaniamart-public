c0809312d7fa1a6e73af9ae5163a57f6edc4735d
Md Wahid Rahman <wahidurrahmanraza@gmail.com>
2025-04-21 18:53:22 +0600

    Update the cors alignment

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,17 @@ const bannerRoutes = require('./routes/bannerRoutes');
 
 const app = express();
 const corsOptions = {
-    origin: 'https://ruhaniamart-public-tgut.vercel.app',
+    // origin: 'https://ruhaniamart-public-tgut.vercel.app',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type']
 };
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
 app.options('*', cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
