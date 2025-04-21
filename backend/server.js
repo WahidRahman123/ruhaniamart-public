842a9c7b4d4b267a270c1cc30616be93f46df2f3
Md Wahid Rahman <wahidurrahmanraza@gmail.com>
2025-04-21 17:36:18 +0600

    Update credentials true

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'https://ruhaniamart-public-tgut.vercel.app',
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type']
 }));
